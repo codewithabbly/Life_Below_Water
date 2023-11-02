856bde3ca2be2d68a198e9020384e2638285dfaf
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2023-11-02 08:32:43 +0800

    live location is on

--- frontend/src/views/InteractiveMapPage.vue
+++ frontend/src/views/InteractiveMapPage.vue
@@ -1,14 +1,14 @@
 <template>
       <div style="height: 100vh; width: 100%;">
-            <l-map :zoom="zoom" :center="center">
+            <l-map :zoom="zoom" :center="center" :options="mapOptions">
                   <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-                  <l-marker :lat-lng="marker"></l-marker>
+                  <l-marker :lat-lng="center"></l-marker>
             </l-map>
       </div>
 </template>
 
 <script>
-      import { LMap, LTileLayer, LMarker } from 'vue3-leaflet';
+      import { LMap, LTileLayer, LMarker } from 'vue3-leaflet'
 
       export default {
             name: 'IMaps',
@@ -23,10 +23,29 @@
                         center: [51.505, -0.09],
                         url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                         attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-                        marker: [51.505, -0.09]
+                        mapOptions: {
+                              maxBounds: [
+                                    [-90, -180],
+                                    [90, 180]
+                              ]
+                        }
                   };
+            },
+            methods: {
+                  setCurrentLocation() {
+                        if (navigator.geolocation) {
+                              navigator.geolocation.getCurrentPosition((position) => {
+                              this.center = [position.coords.latitude, position.coords.longitude]
+                        })
+                        } else {
+                              console.error("Geolocation is not supported by this browser.")
+                        }
+                  }
+            },
+            created() {
+                  this.setCurrentLocation()
             }
-      };
+      }
 </script>
 
 <style>
