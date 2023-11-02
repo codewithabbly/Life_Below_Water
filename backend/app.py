d015d7c3f19839a84423df207f06b9b1b87401d9
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2023-11-02 17:29:10 +0800

    api endpoint for water_data up

---
+++ backend/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+
+client = MongoClient("mongodb+srv://wad2g4t5:Fplyzk5st8wjMfFT@wad2-g4t5.saj8cns.mongodb.net/")
+db = client["wad2-g4t5"]
+collection = db["water_data"]
+
+
+@app.route('/water_data', methods=['GET'])
+def get_water_data():
+      try:
+            all_data = list(collection.find())
+            print(all_data)
+            print("testing")
+            data_list = []
+
+            for data in all_data:
+                  data_dict = {
+                        'country': data["country"],
+                        'water_history': data['water_history'],
+                        'water_pH_level': data['water_pH_level']
+                  }
+
+                  data_list.append(data_dict)
+
+            return jsonify(
+                  {
+                        "code": 200,
+                        "message": "Retrieved all data",
+                        "data": {
+                              "products": data_list
+                  }
+                  }
+            ), 200
+
+      except:
+            return jsonify(
+                  {
+                  "code": 400,
+                  "message": "Unable to retrieve data"
+                  }
+            ), 400
+
+if __name__ == '__main__':
+      app.run(debug=True, port=5006)
