e459278587cc72e84250cff5f1c9e7461c41135b
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2023-11-02 22:07:52 +0800

    base map up but need to include markers

--- frontend/src/views/InteractiveMapPage.vue
+++ frontend/src/views/InteractiveMapPage.vue
@@ -2,25 +2,38 @@
       <div style="height: 100vh; width: 100%;">
             <l-map :zoom="zoom" :center="center" :options="mapOptions">
                   <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-                  <l-marker :lat-lng="center"></l-marker>
+                  <l-marker 
+                        v-for="(item, index) in waterData" 
+                        :key="index" 
+                        :lat-lng="item.latLng" 
+                        @update:click="selectedCity = item"
+                        >
+                        <l-popup v-if="selectedCity === item">
+                              <h2>{{ item.city }}</h2>
+                              <p>{{ item.water_history }}</p>
+                              <p>Water pH Level: {{ item.water_pH_level }}</p>
+                        </l-popup>
+                  </l-marker>
             </l-map>
       </div>
 </template>
 
 <script>
-      import { LMap, LTileLayer, LMarker } from 'vue3-leaflet'
+      import { LMap, LTileLayer } from 'vue3-leaflet'
+      import { server } from "../utils/helper.js"
+      import axios from 'axios'
+      import 'leaflet/dist/leaflet.css'
 
       export default {
             name: 'IMaps',
             components: {
                   LMap,
                   LTileLayer,
-                  LMarker
             },
             data() {
                   return {
                         zoom: 13,
-                        center: [51.505, -0.09],
+                        center: [1.3521, 103.8198],
                         url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                         attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
                         mapOptions: {
@@ -28,23 +41,51 @@
                                     [-90, -180],
                                     [90, 180]
                               ]
-                        }
+                        },
+                        waterData: [],
+                        latLngData: [],
+                        latitude: null,
+                        longitude: null,
+                        selectedCity: null,
                   };
             },
+            async created() {
+                  this.getData()
+                  for (let item of this.waterData) {
+                        item.latLng = await this.getLatLng(item.city)
+                  }
+            },
             methods: {
-                  setCurrentLocation() {
-                        if (navigator.geolocation) {
-                              navigator.geolocation.getCurrentPosition((position) => {
-                              this.center = [position.coords.latitude, position.coords.longitude]
-                        })
-                        } else {
-                              console.error("Geolocation is not supported by this browser.")
+                  getData() {
+                        axios.get(`${server.baseURL}/water_data`)
+                              .then(
+                                    (response) => {
+                                          this.waterData = response.data.data.data_list
+                                    }
+                              ) 
+                              .catch(
+                                    (error) => {
+                                          console.error(error)
+                                    }
+                              )
+                  },
+                  async getLatLng(city) {
+                        try {
+                              const response = await axios.get("https://api.api-ninjas.com/v1/geocoding?city=" + city, {
+                                    headers: {
+                                          'X-Api-Key': 'FEpzYx8KU1vP+l8s/9IZow==WBoyeHhJnShjOtjq'
+                                    }
+                              });
+
+                              this.latLngData = response.data
+                              const firstLocation = this.latLngData[0]
+                              this.latitude= firstLocation.latitude
+                              this.longitude = firstLocation.longitude
+                        } catch (error) {
+                              console.error(error);
                         }
                   }
             },
-            created() {
-                  this.setCurrentLocation()
-            }
       }
 </script>
 
